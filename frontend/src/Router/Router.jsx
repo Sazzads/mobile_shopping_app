4bbf6f55a16d77d2b84bf36c56a349aa2e781036
Sazzads <sazzadtk@gmail.com>
2023-07-15 23:37:11 +0600

    making navbar

--- frontend/src/Router/Router.jsx
+++ frontend/src/Router/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import Home from "../Components/Home";
+import Cart from "../Components/Cart";
 
 const router = createBrowserRouter([
     {
@@ -10,7 +11,11 @@ const router = createBrowserRouter([
             {
                 path:'/',
                 element:<Home></Home>
-            }
+            },
+            {
+                path:'/cart',
+                element:<Cart></Cart>
+            },
         ]
     },
 ]);
